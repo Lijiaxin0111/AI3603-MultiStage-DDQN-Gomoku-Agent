65027eedd4b2492bb02f346abd236ed1f6216a8c
1552140810@qq.com <li_jiaxin@sjtu.edu.cn>
2023-11-16 15:42:30 +0800

    将参数设置和训练分离,可以直接在option.py里面看对应的参数含义,同时方便最后写不同参数的训练脚本script.sh or sript.bat

--- Gomoku+MCTS/config/options.py
+++ Gomoku+MCTS/config/options.py
@@ -25,7 +25,6 @@ parser.add_argument('--gpu_id', type=str, default='5')
 # save options
 parser.add_argument('--clear_visualizer', dest='clear_visualizer', action='store_true')
 parser.add_argument('--std_log', dest='std_log', action='store_true')
-parser.add_argument('--valid_per_epoch', type=int, default=4)
 
 
 # mode settings
@@ -41,12 +40,14 @@ parser.add_argument("--buffer_size",type=int,default=10000,help="The size of col
 parser.add_argument("--batch_size",type=int,default=512)
 
 # ??
-parser.add_argument("--play_batch_size",type=int, default=1,help="??")
+parser.add_argument("--play_batch_size",type=int, default=1,help="The time of selfplaying when collect the data")
 parser.add_argument("--epochs",type=int,default=5,help="num of train_steps for each update")
-parser.add_argument("--kl_targ",type=float,default=0.02,help="??")
-parser.add_argument("--check_freq",type=int,default=50,help='??')
-parser.add_argument("--game_batch_num",type=int,default=1500)
-parser.add_argument("--l2_const",type=float,default=1e-4,help=" coef of l2 penalty")
+parser.add_argument("--kl_targ",type=float,default=0.02,help="the target kl distance between the old decision function and the new decision function ")
+parser.add_argument("--check_freq",type=int,default=50,help='the frequence of the checking the win ratio when training')
+parser.add_argument("--game_batch_num",type=int,default=1500,help =  "the total training times")
+
+
+# parser.add_argument("--l2_const",type=float,default=1e-4,help=" coef of l2 penalty")
 parser.add_argument("--distributed",type=bool,default=False)
 
 # preload_model setting
